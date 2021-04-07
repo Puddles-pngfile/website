aa68b5348dbdda3dc09cb0a19cb1c56588b58a9e
Puddles-pngfile <thomas.munday23@gmail.com>
2021-04-07 12:22:59 +0100

    Random Number generator
    Making a BUTTON on home page, links to this page and generates a random number, add a button on rnadom number page which says "Generate another" whcih refreshes the page therefore it generates a new number

---
+++ m2.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, url_for
+import random
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    name = "Verge"
+    return render_template("index.html")
+
+app.route("/numgen"):
+def numgen():
+    rand = random.randint(0,1000)
+    return render_template("randnum.html")
+    
+
+
+
+
+
+
