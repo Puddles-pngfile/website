949902d9d0920e94a47be91f35d82d2191369053
Puddles-pngfile <thomas.munday23@gmail.com>
2021-04-08 20:50:30 +0100

    Bgcolor
    bgcolor poog

--- main2.py
+++ main2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import random  
 
 
@@ -7,32 +7,45 @@ import random
 
 app = Flask(__name__)
 
-@app.route("/diceroll")
-def dice():
-    return render_template("diceroll.html", dice=random.randint(1,6)
-
-
-
-@app.route("/generator")
-def number():
-    return render_template("number.html", num=random.randint(0,100)
-
-
-
-@app.route("/diceroll")
-def dice():
-    return render_template("diceroll.html", num2=random.randint(1,6))
-
-
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
+
+@app.route("/diceroll")
+def dice():
+    return render_template("diceroll.html", dice=random.randint(1,6))
+
+@app.route("/generator")
+def number():
+    return render_template("number.html", num=random.randint(0,100))
+
+
+
+
+
+
+
+
 @app.route("/joke")
 def joke():
     return render_template("joke.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+
+
 
     
 
