6596c639df67eb205eb2fa8188feeb2519a7e64d
Puddles-pngfile <thomas.munday23@gmail.com>
2021-04-08 20:25:13 +0100

    idk
    Am i supposed to know?

--- main2.py
+++ main2.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template
 import random  
 
+
+
+
+
 app = Flask(__name__)
 
+@app.route("/diceroll")
+def dice():
+    return render_template("diceroll.html", dice=random.randint(1,6)
 
+
+
+@app.route("/generator")
+def number():
+    return render_template("number.html", num=random.randint(0,100)
+
+
+
+@app.route("/diceroll")
+def dice():
+    return render_template("diceroll.html", num2=random.randint(1,6))
 
 
 
@@ -11,14 +29,39 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/generator")
-def number():
-    return render_template("number.html", num=random.randint(0,100))
 
-
-@app.route("/spam")
-def spam():
+@app.route("/joke")
+def joke():
     return render_template("joke.html")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
