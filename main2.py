cdf2de967a68370b58e3eadc33c99c7da4a55376
Puddles-pngfile <thomas.munday23@gmail.com>
2021-04-08 12:38:39 +0100

    JOKE AND NUMBER GEN WORKS!!!! (((((((:::::
    GOT JOKE AND NUMBER GEN TO WORK POGGGG

--- main2.py
+++ main2.py
@@ -5,20 +5,29 @@ app = Flask(__name__)
 
 
 
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
 @app.route("/generator")
 def number():
-    return render_template("number.html", rand = random.randint(0,100)
+    return render_template("number.html", num=random.randint(0,100))
+
+
+@app.route("/spam")
+def spam():
+    return render_template("joke.html")
 
 
 
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
+
 
 
 
