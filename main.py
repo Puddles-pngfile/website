1b754c29cbd14beb53cc7dac0148c41ee8ef280e
Puddles-pngfile <thomas.munday23@gmail.com>
2021-04-16 09:43:30 +0100

    working

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
-from flask import Flask, redirect, url_for, render_template
-import random
+from flask import Flask, render_template, redirect
+import random  
+
+
+
+
+
 app = Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/")
 def home():
-    name = "Verge"
-    return render_template("index.html")
+    return render_template("newhome.html")
 
 
-@app.route("/RNGnumber")
+
+@app.route("/diceroll")
+def dice():
+    return render_template("diceroll.html", dice=random.randint(1,6))
+
+@app.route("/generator")
+def generator():
+    return render_template("number.html", num=random.randint(0,100))
+
+
+@app.route("/gamestore/snake")
 def number():
-    rand = random.randint(0,100)
-    return render_template("randnum.html", rand = rand )
+    return render_template("snake.html")
+
+@app.route("/gamestore")
+def gamestore():
+    return render_template("gamestore.html")
+
+@app.route("/gamestore/tetris")
+def blocky():
+    return render_template("blocky.html")
+
+@app.route("/gamestore/flappybird")
+def race():
+    return render_template("race game.html")
 
 
-if __name__ "__main__":
-    app.run(debug=True)
+@app.route("/diceroll2")
+def dc2():
+    return render_template("cookieclicker.html")
+
+@app.route("/cringe")
+def cringe():
+    return render_template("obama.css")
 
 
 
@@ -24,3 +54,22 @@ if __name__ "__main__":
 
 
 
+
+
+
+
+@app.route("/joke")
+def joke():
+    return render_template("joke.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("aboutus.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
